7525c7d8631c80751e055bb8507d2a824ce2ec16
Wiz <the_wizard_who_says_ni@hotmail.com>
2022-08-24 19:46:37 +0200

    Formatted and labeled PSG instruments.

--- src/data/psg_instruments.asm
+++ src/data/psg_instruments.asm
@@ -1,174 +1,68 @@
-pt_PSG_INSTRUMENTS:dw byte_12D2		; DATA XREF: PSG_LoadInstrument+22o
-					; The PSG instruments only affect the channel level
-		dw byte_12D4		; more details about the data layout in	psg-intruments.txt
-		dw byte_12E6		; instrument 2
-		dw byte_12F4		; instrument 3
-		dw byte_130B		; instrument 4
-		dw byte_1312		; instrument 5
-		dw byte_131D		; instrument 6
-		dw byte_1325		; instrument 7
-		dw byte_132A		; instrument 8
-		dw byte_1338		; instrument 9
-		dw byte_133E		; instrument A
-		dw byte_1349		; instrument B
-		dw byte_134B		; instrument C
-		dw byte_134F		; instrument D
-		dw byte_1355		; instrument E
-		dw byte_135D		; instrument F
-byte_12D2:	db 8Fh			; DATA XREF: RAM:pt_PSG_INSTRUMENTSo
-					; start	of data	: instrument 0
-		db 8Bh
-byte_12D4:	db 0Fh			; DATA XREF: RAM:12B4o
-					; instrument 1
-		db 0Fh
-		db 0Eh
-		db 0Dh
-		db 0Ch
-		db 0Bh
-		db 0Ah
-		db  9
-		db  8
-		db  7
-		db  6
-		db  5
-		db  4
-		db  3
-		db  2
-		db  1
-		db 80h
-		db 80h
-byte_12E6:	db 0Fh			; DATA XREF: RAM:12B6o
-					; instrument 2
-		db 0Eh
-		db 0Eh
-		db 0Dh
-		db 0Dh
-		db 0Dh
-		db 0Ch
-		db 0Ch
-		db 0Ch
-		db 0Ch
-		db 8Bh
-		db 0Ah
-		db 0Ah
-		db 89h
-byte_12F4:	db 0Fh			; DATA XREF: RAM:12B8o
-					; instrument 3
-		db 0Fh
-		db 0Fh
-		db 0Fh
-		db 0Fh
-		db 0Fh
-		db 0Eh
-		db 0Eh
-		db 0Eh
-		db 0Eh
-		db 0Eh
-		db 0Eh
-		db 0Eh
-		db 0Eh
-		db 0Eh
-		db 0Eh
-		db 0Eh
-		db 0Eh
-		db 8Dh
-		db 0Bh
-		db 0Bh
-		db 0Bh
-		db 8Ah
-byte_130B:	db 0Fh			; DATA XREF: RAM:12BAo
-					; instrument 4
-		db 0Fh
-		db 8Eh
-		db 0Bh
-		db 0Bh
-		db 0Bh
-		db 8Ah
-byte_1312:	db 0Fh			; DATA XREF: RAM:12BCo
-					; instrument 5
-		db 0Eh
-		db 8Dh
-		db 0Bh
-		db  9
-		db  7
-		db  5
-		db  3
-		db  2
-		db  1
-		db 80h
-byte_131D:	db 0Dh			; DATA XREF: RAM:12BEo
-					; instrument 6
-		db 0Eh
-		db 0Fh
-		db 8Eh
-		db  4
-		db  2
-		db  1
-		db 80h
-byte_1325:	db 0Dh			; DATA XREF: RAM:12C0o
-					; instrument 7
-		db 0Eh
-		db 0Fh
-		db 8Eh
-		db 8Bh
-byte_132A:	db 0Fh			; DATA XREF: RAM:12C2o
-					; instrument 8
-		db 0Fh
-		db 0Eh
-		db 0Eh
-		db 0Dh
-		db 0Ch
-		db 0Fh
-		db 0Fh
-		db 0Eh
-		db 0Eh
-		db 0Dh
-		db 0Dh
-		db 8Ch
-		db 8Ah
-byte_1338:	db 0Bh			; DATA XREF: RAM:12C4o
-					; instrument 9
-		db 0Ch
-		db 0Dh
-		db 0Eh
-		db 8Fh
-		db 88h
-byte_133E:	db 0Bh			; DATA XREF: RAM:12C6o
-					; instrument A
-		db 0Dh
-		db 0Fh
-		db 0Eh
-		db 0Dh
-		db 8Ch
-		db 0Ah
-		db 0Ah
-		db  9
-		db  9
-		db 88h
-byte_1349:	db 8Fh			; DATA XREF: RAM:12C8o
-					; instrument B
-		db 8Fh
-byte_134B:	db 0Fh			; DATA XREF: RAM:12CAo
-					; instrument C
-		db 0Ah
-		db 86h
-		db 83h
-byte_134F:	db 0Fh			; DATA XREF: RAM:12CCo
-					; instrument D
-		db 0Ch
-		db 0Ah
-		db  8
-		db 86h
-		db 83h
-byte_1355:	db 0Fh			; DATA XREF: RAM:12CEo
-					; instrument E
-		db 0Dh
-		db 0Bh
-		db  9
-		db  8
-		db  7
-		db 86h
-		db 83h
-byte_135D:	db 8Fh			; DATA XREF: RAM:12D0o
-					; instrument F
-		db 80h			; end of data
+
+; Attack-Release volume envelopes
+
+pt_PSG_INSTRUMENTS:
+    dw PSG_INSTRUMENT_0
+    dw PSG_INSTRUMENT_1 
+    dw PSG_INSTRUMENT_2 
+    dw PSG_INSTRUMENT_3 
+    dw PSG_INSTRUMENT_4 
+    dw PSG_INSTRUMENT_5 
+    dw PSG_INSTRUMENT_6 
+    dw PSG_INSTRUMENT_7 
+    dw PSG_INSTRUMENT_8 
+    dw PSG_INSTRUMENT_9 
+    dw PSG_INSTRUMENT_A 
+    dw PSG_INSTRUMENT_B 
+    dw PSG_INSTRUMENT_C 
+    dw PSG_INSTRUMENT_D 
+    dw PSG_INSTRUMENT_E 
+    dw PSG_INSTRUMENT_F 
+PSG_INSTRUMENT_0:  
+    db 8Fh
+    db 8Bh
+PSG_INSTRUMENT_1:  
+    db 0Fh, 0Fh, 0Eh, 0Dh, 0Ch, 0Bh, 0Ah,  9,  8,  7,  6,  5,  4,  3,  2,  1, 80h
+    db 80h
+PSG_INSTRUMENT_2:  
+    db 0Fh, 0Eh, 0Eh, 0Dh, 0Dh, 0Dh, 0Ch, 0Ch, 0Ch, 0Ch, 8Bh
+    db 0Ah, 0Ah, 89h
+PSG_INSTRUMENT_3:
+    db 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 8Dh
+    db 0Bh, 0Bh, 0Bh, 8Ah
+PSG_INSTRUMENT_4:
+    db 0Fh, 0Fh, 8Eh
+    db 0Bh, 0Bh, 0Bh, 8Ah
+PSG_INSTRUMENT_5:
+    db 0Fh, 0Eh, 8Dh
+    db 0Bh,  9,  7,  5,  3,  2,  1, 80h
+PSG_INSTRUMENT_6:
+    db 0Dh, 0Eh, 0Fh, 8Eh
+    db  4,  2,  1, 80h
+PSG_INSTRUMENT_7:
+    db 0Dh, 0Eh, 0Fh, 8Eh
+    db 8Bh
+PSG_INSTRUMENT_8:
+    db 0Fh, 0Fh, 0Eh, 0Eh, 0Dh, 0Ch, 0Fh, 0Fh, 0Eh, 0Eh, 0Dh, 0Dh, 8Ch
+    db 8Ah
+PSG_INSTRUMENT_9:
+    db 0Bh, 0Ch, 0Dh, 0Eh, 8Fh
+    db 88h
+PSG_INSTRUMENT_A:
+    db 0Bh, 0Dh, 0Fh, 0Eh, 0Dh, 8Ch
+    db 0Ah, 0Ah,  9,  9, 88h
+PSG_INSTRUMENT_B:
+    db 8Fh
+    db 8Fh
+PSG_INSTRUMENT_C:
+    db 0Fh, 0Ah, 86h
+    db 83h
+PSG_INSTRUMENT_D:
+    db 0Fh, 0Ch, 0Ah,  8, 86h
+    db 83h
+PSG_INSTRUMENT_E:
+    db 0Fh, 0Dh, 0Bh,  9,  8,  7, 86h
+    db 83h
+PSG_INSTRUMENT_F:
+    db 8Fh
+    db 80h
